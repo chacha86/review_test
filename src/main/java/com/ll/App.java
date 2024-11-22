0ea03e7560e1d8904a279622f9387c088166def0
jhs512 <jangka512@gmail.com>
2024-11-22 12:39:02 +0900

    work

--- src/main/java/com/ll/App.java
+++ src/main/java/com/ll/App.java
@@ -1,18 +1,18 @@
 package com.ll;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class App {
     private final Scanner scanner;
     private int lastId;
-    private final WiseSaying[] wiseSayings;
-    private int wiseSayingsSize;
+    private final List<WiseSaying> wiseSayings;
 
     public App() {
         scanner = new Scanner(System.in);
         lastId = 0;
-        wiseSayings = new WiseSaying[100];
-        wiseSayingsSize = 0;
+        wiseSayings = new ArrayList<>();
     }
 
     public void run() {
@@ -42,8 +42,9 @@ public class App {
 
         WiseSaying wiseSaying = new WiseSaying(id, content, author);
 
-        wiseSayings[wiseSayingsSize] = wiseSaying;
-        wiseSayingsSize++;
+        wiseSayings.add(wiseSaying);
+
+        System.out.println("wiseSayings = " + wiseSayings);
 
         return wiseSaying;
     }
@@ -64,7 +65,6 @@ public class App {
         System.out.println("----------------------");
 
         for (WiseSaying wiseSaying : wiseSayings) {
-            if (wiseSaying == null) break;
             System.out.println("%d / %s / %s".formatted(wiseSaying.getId(), wiseSaying.getAuthor(), wiseSaying.getContent()));
         }
     }
