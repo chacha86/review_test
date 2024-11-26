5cfe618bdc6e2bbfcbb8d1f028042b3b4db1d5af
jhs512 <jangka512@gmail.com>
2024-11-26 10:20:38 +0900

    work

--- src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingRepository.java
+++ src/main/java/com/ll/domain/wiseSaying/repository/WiseSayingRepository.java
@@ -5,14 +5,14 @@ import com.ll.domain.wiseSaying.entity.WiseSaying;
 import java.util.List;
 import java.util.Optional;
 
-public abstract class WiseSayingRepository {
-    public abstract void add(WiseSaying wiseSaying);
+public interface WiseSayingRepository {
+    void add(WiseSaying wiseSaying);
 
-    public abstract List<WiseSaying> findAll();
+    List<WiseSaying> findAll();
 
-    public abstract boolean removeById(int id);
+    boolean removeById(int id);
 
-    public abstract Optional<WiseSaying> findById(int id);
+    Optional<WiseSaying> findById(int id);
 
-    public abstract void modify(WiseSaying wiseSaying);
+    void modify(WiseSaying wiseSaying);
 }
